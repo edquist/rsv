360d332797764046f646941fafbf092d39a46178
Parag Mhashilkar <parag@fnal.gov>
2010-02-25 20:40:11 +0000

    #0 Added cemon-containerkeyfile-ce-permissions-probe probe to check the permissions of container key file used by cemon
    git-svn-id: https://vdt.cs.wisc.edu/svn/rsv/branches/rsv-ress@10165 4e558342-562e-0410-864c-e07659590f8c

--- osg-rsv/bin/probes/RSVProbeBase.pm
+++ osg-rsv/bin/probes/RSVProbeBase.pm
@@ -422,6 +422,10 @@ sub Init_Dependencies {
 	$o{'proxyExpiryWarningHours'}*60*60;          ## Convert to seconds
     $o{'proxyExpiryMinimumSeconds'} = 
 	$o{'proxyExpiryMinimumHours'}*60*60;
+    ## For RESS-CLASSAD-EXISTS-PROBE
+    $o{'ressClassadExistsWorkerScriptFile'} = "$o{'PROBE_DIR_LOCAL'}worker-scripts/ress-classad-exists-probe-worker.sh" if (!defined ($o{'ressClassadExistsWorkerScriptFile'}));
+    ## For CEMON-CONTAINERKEYFILE-CE-PERMISSIONS-PROBE
+    $o{'cemoncontainerkeyfileWorkerScriptFile'} = "$o{'PROBE_DIR_LOCAL'}worker-scripts/cemon-containerkeyfile-ce-permissions-worker" if (!defined ($o{'cemoncontainerkeyfileWorkerScriptFile'}));
 
     
     ## GLOBUS STUFF
