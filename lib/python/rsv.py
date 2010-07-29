0c0b19afc6230c9a5a110a697ebc360f5d636031
Scot Kronenfeld <kronenfe@cs.wisc.edu>
2010-07-29 16:48:25 +0000

    #0 Merge r10708 and r10709 to this branch.  They were accidently committed to a tag instead.
    git-svn-id: https://vdt.cs.wisc.edu/svn/rsv/branches/rsv-v4@10710 4e558342-562e-0410-864c-e07659590f8c

--- lib/python/rsv.py
+++ lib/python/rsv.py
@@ -22,6 +22,8 @@ CONFIG  = ConfigParser.RawConfigParser()
 OPTIONS = None
 RSV_LOC = None
 
+OPENSSL_EXE = "/usr/bin/openssl"
+
 
 def initialize():
     """ Handle the command line, load configuration files and do other basic
@@ -240,8 +242,8 @@ def renew_service_certificate_proxy(cert, key, proxy):
 
     hours_til_expiry = 6
     seconds_til_expiry = hours_til_expiry * 60 * 60
-    (ret, out) = utils.system("/usr/bin/openssl x509 -in %s -noout -enddate -checkend %s" %
-                              (proxy, seconds_til_expiry))
+    (ret, out) = utils.system("%s x509 -in %s -noout -enddate -checkend %s" %
+                              (OPENSSL_EXE, proxy, seconds_til_expiry))
     
     if ret == 0:
         log("Service certificate valid for at least %s hours." % hours_til_expiry, 2, 4)
@@ -250,11 +252,16 @@ def renew_service_certificate_proxy(cert, key, proxy):
             hours_til_expiry, 2, 4)
 
         grid_proxy_init_exe = os.path.join(OPTIONS.vdt_location, "globus", "bin", "grid-proxy-init")
-        (ret, out) = utils.system("%s -cert %s %s -key %s -valid 12:00 -debug -out %s" %
+        (ret, out) = utils.system("%s -cert %s -key %s -valid 12:00 -debug -out %s" %
                                   (grid_proxy_init_exe, cert, key, proxy))
 
         if ret:
             results.service_proxy_renewal_failed(cert, key, proxy, out)
+
+    # Globus needs help finding the service proxy since it probably does not have the
+    # default naming scheme of /tmp/x509_u<UID>
+    os.environ["X509_USER_PROXY"] = proxy
+    os.environ["X509_PROXY_FILE"] = proxy
 
     return
 
@@ -274,10 +281,14 @@ def check_user_proxy(proxy_file):
     # so this check might need to be adjusted if that behavior is more understood.
     minutes_til_expiration = 10
     seconds_til_expiration = minutes_til_expiration * 60
-    (ret, out) = utils.system("/usr/bin/openssl x509 -in %s -noout -enddate -checkend %s" %
-                              (proxy_file, seconds_til_expiration))
+    (ret, out) = utils.system("%s x509 -in %s -noout -enddate -checkend %s" %
+                              (OPENSSL_EXE, proxy_file, seconds_til_expiration))
     if ret:
         results.expired_user_proxy(proxy_file, out, minutes_til_expiration)
+
+    # Just in case this isn't the default /tmp/x509_u<UID> we'll explicitly set it
+    os.environ["X509_USER_PROXY"] = proxy_file
+    os.environ["X509_PROXY_FILE"] = proxy_file
 
     return
 
